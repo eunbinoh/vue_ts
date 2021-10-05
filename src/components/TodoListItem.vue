d2f7c19bb6016672b94c1de02345092cdf480ae2
eunbinoh <nohonbi01@gmail.com>
2021-10-05 17:29:38 +0900

    vue_todo_ts project example

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <li>
+      <span class="item" :class="todoItem.done ? 'complete' : null" 
+      @click="toggleItem"> {{ todoItem.title }} </span>
+      <button @click="removeItem">삭제</button>
+    </li>
+</template>
+
+<script lang="ts">
+import { Todo } from "@/App.vue";
+import Vue, { PropType } from "vue"
+
+export default Vue.extend({
+    props:{
+        todoItem: Object as PropType<Todo>,
+        index: Number
+    },
+    methods: {
+        toggleItem(){
+            this.$emit("toggle", this.todoItem, this.index);
+        },
+        removeItem(){
+            this.$emit("remove", this.index);
+        }
+    }
+});
+</script>
+
+<style scoped>
+.item{
+    cursor: pointer;
+}
+ .complete{
+     text-decoration: line-through;
+ }
+</style>
